abfcdb6e7496bf732b76b4d479999769f0234009
Anjaneya0123 <anjaneya.er@gmail.com>
2025-07-24 20:46:21 +0530

    Add files via upload

---
+++ jst-ticket-booking-spring-boot-main_userservice/src/main/java/com/jsp/jst_ticket_booking_spring_boot/JstTicketBookingSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.jsp.jst_ticket_booking_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+
+public class JstTicketBookingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstTicketBookingSpringBootApplication.class, args);
+	}
+
+}
---
+++ jst-ticket-booking-spring-boot-main_userservice/src/main/java/com/jsp/jst_ticket_booking_spring_boot/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.jsp.jst_ticket_booking_spring_boot.repository;
+
+import com.jsp.jst_ticket_booking_spring_boot.dto.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
+    static Customer findByEmail(String email) {
+        return null;
+    }
+}
